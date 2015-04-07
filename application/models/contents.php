6173d3c96cd3676685bd4e39854e2c67d87b4a68
owen <myartame@gmail.com>
2015-04-07 16:46:48 +0900

    modify contents_get

--- application/models/contents.php
+++ application/models/contents.php
@@ -9,8 +9,19 @@ class Contents extends CI_Model{
 
 	public function get($state, $limit, $offset){
 		$current_time = date("Y-m-s h:m:s");
-		return $this->db->select('id, subject, url, play_time')->from('Contents')->where('state', $state)->
+		
+		$contents = $this->db->select('id, subject, url, play_time')->from('Contents')->where('state', $state)->
 			//where(sprintf('start_time <= %s and %s <= end_time', $current_time, $current_time))->
 			limit($limit, $offset)->get()->result();
+		foreach ($contents as $value) {
+			$value->tag = $this->db->select('name')->from('tag')->
+				where('contents_id', $value->id)->get()->result();
+		}
+
+		return $contents;
+	}
+
+	public function search($kind){
+
 	}
 }
